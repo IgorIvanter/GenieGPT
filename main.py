27eea7096a021ed6550b224c7c8be36df23d28f8
Stokes machine <igor.ivanter@gmail.com>
2023-03-19 15:58:51 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+from telegram.ext import Updater, MessageHandler, Filters
+import telegram
+import openai
+from moviepy.editor import AudioFileClip
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Load environment variables from .env file
+
+openai.api_key = os.getenv('OPENAI_API_KEY')
+TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+
+messages = [{"role": "system", "content": "You are SuperTelegramGPT, a helpful telegram bot who is also extremely funny and a very cocky, and likes to troll people a bit and show character, but you still remain very helpful and you strive to fulfill all user's requests. You are a powerful creature with ears so you can hear if a user sends you a telegram voice note."}]
+
+def text_message(update, context):
+    messages.append({"role": "user", "content": update.message.text})
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+    ChatGPT_reply = response["choices"][0]["message"]["content"]
+    update.message.reply_text(text=f"*[Bot]:* {ChatGPT_reply}", parse_mode=telegram.ParseMode.MARKDOWN)
+    messages.append({"role": "assistant", "content": ChatGPT_reply})
+
+
+def voice_message(update, context):
+    update.message.reply_text("I've received a voice message! Please give me a second to respond :)")
+    voice_file = context.bot.getFile(update.message.voice.file_id)
+    voice_file.download("voice_message.ogg")
+    audio_clip = AudioFileClip("voice_message.ogg")
+    audio_clip.write_audiofile("voice_message.mp3")
+    audio_file = open("voice_message.mp3", "rb")
+    transcript = openai.Audio.transcribe("whisper-1", audio_file).text
+    update.message.reply_text(text=f"*[You]:* _{transcript}_", parse_mode=telegram.ParseMode.MARKDOWN)
+    messages.append({"role": "user", "content": transcript})
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+    ChatGPT_reply = response["choices"][0]["message"]["content"]
+    update.message.reply_text(text=f"*[Bot]:* {ChatGPT_reply}", parse_mode=telegram.ParseMode.MARKDOWN)
+    messages.append({"role": "assistant", "content": ChatGPT_reply})
+
+
+updater = Updater(TELEGRAM_BOT_TOKEN, use_context=True)
+dispatcher = updater.dispatcher
+dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command), text_message))
+dispatcher.add_handler(MessageHandler(Filters.voice, voice_message))
+updater.start_polling()
+updater.idle()
