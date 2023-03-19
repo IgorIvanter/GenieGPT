e27718dc45d41729053439b0765535d5117ed3c7
Stokes machine <igor.ivanter@gmail.com>
2023-03-19 20:00:14 +0100

    added logging, added a full reqs file

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ import openai
 from moviepy.editor import AudioFileClip
 from dotenv import load_dotenv
 import os
+import logging
 
 load_dotenv()  # Load environment variables from .env file
-
 openai.api_key = os.getenv('OPENAI_API_KEY')
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 messages = [{"role": "system", "content": "You are SuperTelegramGPT, a helpful telegram bot who is also extremely funny and a very cocky, and likes to troll people a bit and show character, but you still remain very helpful and you strive to fulfill all user's requests. You are a powerful creature with ears so you can hear if a user sends you a telegram voice note."}]
 
