dc5c0e41bbee2a9828ebf7b3d8943802759271fe
Stokes machine <igor.ivanter@gmail.com>
2023-03-20 23:02:04 +0100

    added fully functioning retry button. all the previous finctionality is preserved. deleted redundant code. retrybutton only appears  after a timeout error (for certainty). if you press retry, the last message gets sent to the bot agagin. the old one doesn't get removed from the history (TODO. i'm not sure if an error can happen with len(history) == 1, need to think about that).

--- main.py
+++ main.py
@@ -24,57 +24,35 @@ DEFAULT_SYSTEM_MESSAGE = "You are GenieGPT, a helpful telegram bot who is also e
 
 WELCOME_MESSAGE = "*Welcome to GenieGPT!*\n\n*Who am I?*\n\nI am GenieGPT - a powerful AI system, I analyze your message and provide you with a helpful response as quickly as possible. You can also send me voice notes and I will hear you (yeah I have ears 😱)\n\n*What can I do?*\n\nFrom writing essays on classic literature to explaining quantum field theory - you name it. Just type in your question and I will get back to you ASAP 😎\n\n*Limitations*\n\nPlease keep in mind that sometimes I may not answer because I am overloaded with requests from other users."
 
-OPENAI_REQUEST_TIMEOUT = 60  # openai request timeout in seconds
-
-
-# Define the callback functions for the buttons
-def button1_callback(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text=f"You pressed button 1. Update: {context.bot.get_updates(offset=update.update_id - 3)}")
-    print(context.bot.get_updates(offset=update.update_id - 3))
-
-
-def button_submit_callback(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text=f"You pressed button 2. Update: {update.message.text}")
+OPENAI_REQUEST_TIMEOUT = 4  # openai request timeout in seconds
 
 
 def button_retry_callback(update, context):
+    print("Entering retry callback")
+    print(update)
     last_request = context.user_data.get("last_request")
     context.bot.send_message(chat_id=update.effective_chat.id,
                              text=f"*[You]:* {last_request}")
     last_error_message = context.user_data['last_error_message']
     context.bot.edit_message_text(chat_id=last_error_message.chat.id,
                                message_id=last_error_message.message_id, text=last_error_message.text)
+    last_update = context.user_data['last_update']
+    text_message(last_update, context)
 
 
 def button_click_handler(update, context):
     query = update.callback_query
     query.answer()
     button_id = query.data
-    if button_id == 'button1':
-        button1_callback(update, context)
-    elif button_id == 'button2':
-        button_submit_callback(update, context)
-    elif button_id == 'retry':
+    if button_id == 'retry':
         button_retry_callback(update, context)
     else:
         query.edit_message_text(text="Invalid button")
 
 
-def buttons_demo(update, context):
-    # Create the inline keyboard with a button
-    inline_keyboard = [[InlineKeyboardButton(
-        'Button 1', callback_data='button1'), InlineKeyboardButton('Button 2', callback_data='button2')]]
-    reply_markup = InlineKeyboardMarkup(inline_keyboard)
-
-    # Add the keyboard to the message
-    reply_markup = InlineKeyboardMarkup(inline_keyboard)
-    update.message.reply_text('Press a button:', reply_markup=reply_markup)
-
-
 def text_message(update, context):
     context.user_data['last_request'] = update.message.text
+    context.user_data['last_update'] = update
     debug_message = ""
     if context.user_data.get('messages') == None:
         context.user_data["messages"] = [
@@ -120,14 +98,11 @@ def voice_message(update, context):
     audio_file = open("voice_message.mp3", "rb")
     transcript = openai.Audio.transcribe("whisper-1", audio_file).text
     update.message.text = transcript
-    inline_keyboard = [[InlineKeyboardButton(
-        'Edit', callback_data='button1'), InlineKeyboardButton('Submit', callback_data='button2')]]
-    reply_markup = InlineKeyboardMarkup(inline_keyboard)
     context.bot.delete_message(
         chat_id=update.message.chat_id,
         message_id=voice_received_message.message_id)
     update.message.reply_text(
-        text=f"*[You]:* _{transcript}_", parse_mode=telegram.ParseMode.MARKDOWN, reply_markup=reply_markup)
+        text=f"*[You]:* _{transcript}_", parse_mode=telegram.ParseMode.MARKDOWN)
     text_message(update, context)
 
 
@@ -140,6 +115,10 @@ def error(update, context):
     raise EnvironmentError
 
 
+def timeout_error(update, context):
+    raise openai.error.Timeout
+
+
 def error_handler(update, context):
     print("Entering error handler", sep='\n')
     print(update, sep='\n')
@@ -147,15 +126,13 @@ def error_handler(update, context):
     inline_keyboard = [[InlineKeyboardButton('Retry', callback_data='retry')]]
     reply_markup = InlineKeyboardMarkup(inline_keyboard)
     if isinstance(context.error, openai.error.Timeout):
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Hey there, I'm sorry, but I couldn't get you an answer in reasonable time.\n\nThis might be because too many users are trying to get a response.\n\nYou can repeat your request and I will do my best to get you an answer this time 😎.\n\nYour chat history isn't affected by this error.", reply_markup=reply_markup)
+        error_message = context.bot.send_message(chat_id=update.effective_chat.id, text="Hey there, I'm sorry, but I couldn't get you an answer in reasonable time.\n\nThis might be because too many users are trying to get a response.\n\nYou can repeat your request and I will do my best to get you an answer this time 😎.\n\nYour chat history isn't affected by this error.", reply_markup=reply_markup)
+        context.user_data["last_error_message"] = error_message
     elif isinstance(context.error, telegram.error.NetworkError):
         print("\n\n\n\n----------------------------------------------------------------\n----------------------------------------------------------------\nINTERNET FAIL!!!!!!!\n----------------------------------------------------------------\n----------------------------------------------------------------\n")
     else:
-        error_message = context.bot.send_message(chat_id=update.effective_chat.id,
-                                                 text=f"Sorry, an error occurred: '{str(context.error)}'.\n\nIf it's something strange please contact @igor_ivanter for questions.\n\nType: {type(context.error)}.\n\nTrying to print: {context.error}", reply_markup=reply_markup)
-        # update.message.reply_text(
-        #     f"Now update of the message above: {error_message}")
-        context.user_data["last_error_message"] = error_message
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                                 text=f"Sorry, an error occurred: '{str(context.error)}'.\n\nIf it's something strange please contact @igor_ivanter for questions.\n\nType: {type(context.error)}.\n\nTrying to print: {context.error}")
 
 
 def main():
@@ -163,10 +140,10 @@ def main():
     dispatcher = updater.dispatcher
 
     # add the command handlers
-    dispatcher.add_handler(CommandHandler('buttons', buttons_demo))
     dispatcher.add_handler(CommandHandler('start', start))
     dispatcher.add_handler(CommandHandler('help', start))
     dispatcher.add_handler(CommandHandler('error', error))
+    dispatcher.add_handler(CommandHandler('timeout', timeout_error))
 
     # add the text message hadler
     dispatcher.add_handler(MessageHandler(
