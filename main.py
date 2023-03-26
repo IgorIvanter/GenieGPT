ee581bd04f2d776e73743f23aa5fb661e1683266
Stokes machine <igor.ivanter@gmail.com>
2023-03-26 16:44:39 +0200

    extracted error handler into separate file

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from telegram.ext import Updater, MessageHandler, CommandHandler, Filters, CallbackQueryHandler
 import telegram
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 import openai
 from dotenv import load_dotenv
 import os
@@ -12,6 +11,9 @@ from commands import handle_command_help
 from commands import handle_command_start
 from commands import handle_command_timeout
 from commands import handle_command_error
+
+# import error handler
+from error import handle_error
 
 # import message handlers
 from handlers import handle_message_text
@@ -46,20 +48,6 @@ def handle_callback_query(update, context):
         button_callback_retry(update, context)
     else:
         query.edit_message_text(text="Invalid button")
-
-
-def handle_error(update, context):
-    inline_keyboard = [[InlineKeyboardButton('Retry', callback_data='retry')]]
-    reply_markup = InlineKeyboardMarkup(inline_keyboard)
-    if isinstance(context.error, openai.error.Timeout):
-        error_message = context.bot.send_message(
-            chat_id=update.effective_chat.id, text="Hey there, I'm sorry, but I couldn't get you an answer in reasonable time.\n\nThis might be because too many users are trying to get a response.\n\nYou can repeat your request and I will do my best to get you an answer this time 😎.\n\nYour chat history isn't affected by this error.", reply_markup=reply_markup)
-        context.user_data["last_error_message"] = error_message
-    elif isinstance(context.error, telegram.error.NetworkError):
-        print("\n\n\n\n----------------------------------------------------------------\n----------------------------------------------------------------\nINTERNET FAIL!!!!!!!\n----------------------------------------------------------------\n----------------------------------------------------------------\n")
-    else:
-        context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text=f"Sorry, an error occurred: '{str(context.error)}'.\n\nIf it's something strange please contact @igor_ivanter for questions.\n\nType: {type(context.error)}.\n\nTrying to print: {context.error}")
 
 
 def main():
