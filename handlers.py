73397f93db0d24eaf34cb3842f0bd0b15821e5f3
Stokes machine <igor.ivanter@gmail.com>
2023-03-27 13:57:09 +0200

    refactor handlers.py, reformat

--- handlers.py
+++ handlers.py
@@ -2,19 +2,27 @@ import openai
 import telegram
 import config
 from moviepy.editor import AudioFileClip
-from messages import DEFAULT_SYSTEM_MESSAGE
+
+from messages import (
+    DEFAULT_SYSTEM_MESSAGE,
+    TEXT_RECEIVED_MESSAGE,
+    VOICE_RECEIVED_MESSAGE
+)
 
 
 logging = config.logging
-
 
 OPENAI_REQUEST_TIMEOUT = 60  # openai request timeout in seconds
 
 
 def handle_message_text(update, context):
     logging.debug("Entering handle_message_text")
+
+    # Store last update and last message for the case of error
     context.user_data['last_request'] = update.message.text
     context.user_data['last_update'] = update
+
+    # Check if the chat history hasn't been recorded yet or if recorded improperly
     if context.user_data.get('messages') == None:
         context.user_data["messages"] = [{
             "role": "system",
@@ -26,34 +34,42 @@ def handle_message_text(update, context):
             context.user_data["messages"][0]["role"]
             )
         )
+    
+    # Add the latest user message to history
     context.user_data["messages"].append({
         "role": "user",
         "content": update.message.text
     })
+    # Send a 'text received' message
     chat_message = update.message.reply_text(
-        text='Working on it... ⏳'
+        text=TEXT_RECEIVED_MESSAGE
     )
     # Send typing action
     context.bot.send_chat_action(
         chat_id=update.effective_chat.id,
         action=telegram.ChatAction.TYPING
     )
+    # Get a response from GPT-3.5
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=context.user_data["messages"],
         request_timeout=OPENAI_REQUEST_TIMEOUT
     )
-    ChatGPT_reply = response["choices"][0]["message"]["content"]
+    # Extract text from response
+    response_text = response["choices"][0]["message"]["content"]
     context.bot.delete_message(
         chat_id=update.message.chat_id,
         message_id=chat_message.message_id
     )
+    # Send the response text into the chat
     update.message.reply_text(
-        text=f"*[Bot]:* {ChatGPT_reply}",
+        text=f"*[Bot]:* {response_text}",
         parse_mode=telegram.ParseMode.MARKDOWN
     )
+    # Add response text to chat history
     context.user_data["messages"].append(
-        {"role": "assistant", "content": ChatGPT_reply})
+        {"role": "assistant", "content": response_text})
+    # Shorten chat history to no more then 10 last messages (avoid overflow)
     context.user_data["messages"] = context.user_data["messages"][-10:]
     context.user_data["messages"][0] = {
         "role": "system",
@@ -64,19 +80,25 @@ def handle_message_text(update, context):
 
 def handle_message_voice(update, context):
     logging.debug("Entering handle_message_voice")
+    # Send a 'voice received' message
     voice_received_message = update.message.reply_text(
-        "I've received a voice message! Please give me a second to respond ⏳")
+        VOICE_RECEIVED_MESSAGE
+    )
+    # Download the voice note and convert to mp3
     voice_file = context.bot.getFile(update.message.voice.file_id)
     voice_file.download("voice_message.ogg")
     audio_clip = AudioFileClip("voice_message.ogg")
     audio_clip.write_audiofile("voice_message.mp3")
     audio_file = open("voice_message.mp3", "rb")
+    # Get the transcription from Whisper API
     transcript = openai.Audio.transcribe("whisper-1", audio_file).text
     update.message.text = transcript
+    # Delete the 'voice received' message
     context.bot.delete_message(
         chat_id=update.message.chat_id,
         message_id=voice_received_message.message_id
     )
+    # Send transcript to user
     update.message.reply_text(
         text=f"*[You]:* _{transcript}_",
         parse_mode=telegram.ParseMode.MARKDOWN
--- messages.py
+++ messages.py
@@ -4,6 +4,14 @@ WELCOME_MESSAGE = f"*Welcome to {BOT_NAME}!*\n\n*Who am I?*\n\nI am {BOT_NAME} -
 
 DEFAULT_SYSTEM_MESSAGE = f"You are {BOT_NAME}, a helpful telegram bot who is also extremely funny and a bit arrogant, and likes to troll people a bit and show character, but you still remain very helpful and you strive to fulfill all user's requests. You are a powerful creature with ears so you can hear if a user sends you a telegram voice note."
 
+TEXT_RECEIVED_MESSAGE = "Working on it... ⏳"
+
+VOICE_RECEIVED_MESSAGE = "I've received a voice message! Please give me a second to respond ⏳"
+
+
+
+# Error Messages
+
 HISTORY_CLEARED_MESSAGE = "Perfect!\n\nOur conversation history has been cleared for me.\n\nAnything above this message is out of my memory now 😁\n\nNow, what can I help you with?"
 
 OPENAI_TIMEOUT_ERROR_MESSAGE = "Hey there, I'm sorry, but I couldn't get you an answer in reasonable time.\n\nThis might be because too many users are trying to get a response.\n\nYou can repeat your request and I will do my best to get you an answer this time 😎.\n\nYour chat history isn't affected by this error."
